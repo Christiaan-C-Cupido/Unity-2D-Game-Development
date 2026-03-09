8f4701995ece65ce4d59eb1951345866266b01e9
Christiaan C Cupido <ccccupido@uwc.ac.za>
2026-03-09 14:32:37 +0200

    Delivery Dash

--- Unity-2D-Game-Development/Assets/Scripts/Driver.cs
+++ Unity-2D-Game-Development/Assets/Scripts/Driver.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        float moveSpeed = 0.05f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, 0, 0.5f);
+        transform.Translate(0, moveSpeed, 0);
     }
 }
