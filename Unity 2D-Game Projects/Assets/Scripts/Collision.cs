3d0bbb4591482729fc2eccf427568b61158683c4
Christiaan C Cupido <ccccupido@uwc.ac.za>
2026-03-23 13:33:27 +0200

    Keyboard inputs

---
+++ Unity 2D-Game Projects/Assets/Scripts/Collision.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Hit");
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger!");   
+    }
+}
--- Unity 2D-Game Projects/Assets/Scripts/Driver.cs
+++ Unity 2D-Game Projects/Assets/Scripts/Driver.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Driver : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        transform.Rotate(0, 0, 45);
-    }
-
+    [SerializeField] float steerspeed = 0.5f;
+    [SerializeField] float moveSpeed = 0.1f;
+    
+ 
     // Update is called once per frame
     void Update()
     {
+        float steer = 0f;
+        float move = 0f;
         
+        if (Keyboard.current.aKey.isPressed)
+        {
+            steer = 1f;
+        }
+
+        else if (Keyboard.current.dKey.isPressed)
+        {
+            steer = -1f;
+        }
+
+        if (Keyboard.current.wKey.isPressed)
+        {
+            move = 1f;
+        }
+
+        else if (Keyboard.current.sKey.isPressed)
+        {
+            move = -1f;
+        }
+
+        float moveAmount = move * moveSpeed * Time.deltaTime;
+        float steerAmount = steer * steerspeed * Time.deltaTime;
+
+        transform.Rotate(0, 0, steerAmount);
+        transform.Translate(0, moveAmount, 0f);
     }
 }
